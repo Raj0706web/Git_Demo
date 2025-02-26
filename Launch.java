da7b4eabf05aeab744a5c359f85247b6efcb7dab
Ankit Raj <ankitraj9905680581@gmail.com>
2025-02-26 23:37:03 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,5 +3,12 @@ class Launch
 	public static void main(String[]args)
 	{
 		System.out.println("Exploring Git & GitHub!");
+		
+	}
+	public static void fun()
+	{
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
 	}
 }
